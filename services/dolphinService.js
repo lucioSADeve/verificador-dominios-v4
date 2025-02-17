2b8f8f0848213e585a1399c787ca16f5b038645f
lucioSADeve <luciocamposmarcio@gmail.com>
2025-02-17 20:51:50 -0300

    Atualizado serviço Dolphin com endpoints e parâmetros corretos da API

--- services/dolphinService.js
+++ services/dolphinService.js
@@ -15,13 +15,22 @@ class DolphinService {
         });
     }
 
-    // Obter todos os perfis disponíveis
-    async getProfiles() {
+    /**
+     * Obter lista de perfis com filtros
+     * @param {Object} params Parâmetros de filtro
+     * @param {number} params.limit Quantidade de perfis por página (max 50)
+     * @param {string} params.query Busca por nome do perfil
+     * @param {string[]} params.tags Array de tags
+     * @param {string[]} params.statuses Array de IDs de status
+     * @param {string[]} params.mainWebsites Sites principais ['facebook', 'google', 'crypto', 'tiktok']
+     * @param {number[]} params.users Array de IDs de usuários
+     * @param {number} params.page Número da página
+     */
+    async getProfiles(params = {}) {
         try {
             console.log('Obtendo perfis do Dolphin Anty...');
             
-            // Endpoint correto
-            const response = await this.api.get('/browser_profiles');
+            const response = await this.api.get('/browser_profiles', { params });
             console.log('Resposta:', response.data);
             return response.data;
         } catch (error) {
@@ -33,13 +42,28 @@ class DolphinService {
         }
     }
 
-    // Iniciar perfil (endpoint da documentação)
+    /**
+     * Obter um perfil específico
+     * @param {string} profileId ID do perfil
+     */
+    async getProfile(profileId) {
+        const response = await this.api.get(`/browser_profiles/${profileId}`);
+        return response.data;
+    }
+
+    /**
+     * Iniciar um perfil
+     * @param {string} profileId ID do perfil
+     */
     async startProfile(profileId) {
         const response = await this.api.post(`/browser_profiles/${profileId}/start`);
         return response.data;
     }
 
-    // Criar uma nova aba (endpoint da documentação)
+    /**
+     * Criar uma nova aba
+     * @param {string} profileId ID do perfil
+     */
     async createTab(profileId) {
         const response = await this.api.post(`/browser_profiles/${profileId}/tabs`);
         return response.data;
